041ecd19932524c781a6eb33cf3c520569e944e1
daejoon <kkode1911@gmail.com>
2023-04-08 13:35:02 +0900

    Added link.svg

---
+++ src/main/kotlin/com/ddoong2/gitautolink/ui/MyIcons.kt
@@ -0,0 +1,8 @@
+package com.ddoong2.gitautolink.ui
+
+import com.intellij.openapi.util.IconLoader
+
+object MyIcons {
+    @JvmField
+    val LINK = IconLoader.getIcon("/icons/link.svg", javaClass)
+}
